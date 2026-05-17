5b026b83fa15466ba1088d8a6ab40a86af7a15ee
Lolaaos <todocapa1900@gmail.com>
2026-05-16 21:18:39 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,11 @@
 // Separa los IDs con comas.
 
 export const ADMIN_IDS = [
-  "269902224732192768", "884858860291117066" //  Tu ID principal
+  "269902224732192768", // Tu ID principal
+  "884858860291117066"  // Otro admin
 ];
 
+// URL del webhook de Discord para notificaciones de compra
 export const PURCHASE_WEBHOOK_URL =
   process.env.DISCORD_PURCHASE_WEBHOOK ||
   "https://discord.com/api/webhooks/1505336716612079636/Z5Xit34G-p5Qh4zuTbw2kgXlEKJK3hTkXiYb5u1yaASxURwv9SVIZPm2SVVFmNLoYJn9";
